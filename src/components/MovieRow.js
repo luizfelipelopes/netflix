4dcd6a5d66d004b5ceb27cc08a415a416d298256
Luiz <luiz.felipe.c.lopes@accenture.com>
2021-06-26 18:43:36 -0300

    estilizacao itens e background

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -4,12 +4,17 @@ import './MovieRow.css';
 export default ({title, items}) => {
 
     return (
-        <div>
+        <div className="movieRow">
             <h2>{title}</h2>
             <div className="movieRow--listarea">
-                {typeof items.success === 'undefined' && items.results.length > 0 && items.results.map((item, key) => (
-                    <img src={'https://image.tmdb.org/t/p/w300' + item.poster_path} alt={item.original_title}/>
-                ))}
+                <div className="movieRow--list">
+                    {typeof items.success === 'undefined' && items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movieRow--item">
+                            <img src={'https://image.tmdb.org/t/p/w300' + item.poster_path} alt={item.original_title}/>
+                        </div>
+                        
+                    ))}
+                </div> 
             </div>
         </div>
     );
